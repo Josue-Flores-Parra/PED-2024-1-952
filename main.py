851c1189b6d245e60c5ad8d93aa697569eb24bc6
alumnoFCA <josue.miguel.flores.parra@uabc.edu.mx>
2024-04-09 19:18:30 -0700

    Cree función segundo_mensaje

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 # Imprimir mensajes
 
+def segundo_mensaje():
+    print("otra cosa !!!")
+
+
 def otro_mensaje():
     print("otro mensaje!!!")
+
 
 if __name__ == "__main__":
     print("Hello world!")
     otro_mensaje()
+    segundo_mensaje()
